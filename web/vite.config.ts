c2d210b7f8de1eec111a2395ecf40d5b46d2c9fd
井上翼 <tsubasa0129ti@gmail.com>
2024-03-06 20:27:51 +0900

    feat: v001 viteの出力先、サーバーの設定を行なった

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { splitVendorChunkPlugin, defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 import path from 'path';
@@ -8,15 +8,48 @@ const root = path.resolve(__dirname, 'src');
 // https://vitejs.dev/config/
 export default defineConfig({
   root: root,
-  plugins: [vue()],
+  plugins: [vue(), splitVendorChunkPlugin()],
+  resolve: {
+    alias: {
+      // import '/@component/sample.vue'からモジュールを使用可能にする。
+      '/@component/': path.join(__dirname, 'src/main/vue/component'),
+    }
+  },
+  server: {
+    host: true, // dockerなどの仮想から外に出すため（ポートを開けるという意味かな？）
+    port: 3000,
+    strictPort: true,
+    watch: {
+      usePolling: true
+    }
+  },
   build: {
     outDir: '../../src/main/resources/',
-    assetsDir: 'static',
+    // emptyOutDir: true,
     rollupOptions: {
       input: {
         'templates/index': path.resolve(__dirname, 'src/templates/index.html'),
         'templates/db_sample.html': path.resolve(__dirname, 'src/templates/db_sample.html')
-      }
+      },
+      output: {
+        entryFileNames: (entryFile) => {
+          /**
+           * NOTE: templatesディレクトリから一段階外に出している。かなり無理やり実装しているので、今後修正する。
+           */
+          let name = entryFile.name.replace('templates/', '');
+          return `static/js/${name}.js`;
+        },
+        chunkFileNames: `static/vendor/[hash].js`,
+        assetFileNames: (asset) => {
+          if (/\.( gif|jpeg|jpg|png|svg|webp| )$/.test(asset.name!)) {
+            return 'static/assets/[name].[ext]';
+          }
+          if (/\.css$/.test(asset.name!)) {
+            return `static/css/[name].css`;
+          }
+          return 'static/assets/[name].[ext]';
+        }
+      },
     }
   }
 })
