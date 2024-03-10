6c15865054ae8c0a6fdfbb5cf1c87e392018233d
井上翼 <tsubasa0129ti@gmail.com>
2024-03-10 18:35:59 +0900

    feat: v001 viteのプラグインでの型チェックを有効にした

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,5 +1,6 @@
 import { splitVendorChunkPlugin, defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import checker from "vite-plugin-checker";
 
 import path from 'path';
 import fs from 'fs';
@@ -29,46 +30,58 @@ const entryFiles = glob.sync("**/*.html", {
 
 const entryObject = Object.fromEntries(entryFiles);
 
-export default defineConfig({
-  root: root,
-  plugins: [vue(), splitVendorChunkPlugin()],
-  resolve: {
-    alias: {
-      // import '/@component/sample.vue'からモジュールを使用可能にする。
-      '/@component/': path.join(__dirname, 'src/main/vue/component'),
-    }
-  },
-  server: {
-    host: true, // dockerなどの仮想から外に出すため（ポートを開けるという意味かな？）
-    port: 3000,
-    strictPort: true,
-    watch: {
-      usePolling: true
-    }
-  },
-  build: {
-    outDir: '../../src/main/resources/',
-    rollupOptions: {
-      input: entryObject,
-      output: {
-        entryFileNames: (entryFile) => {
-          /**
-           * NOTE: templatesディレクトリから一段階外に出している。
-           */
-          const name = cutFromBeginning(entryFile.name, 'templates/');
-          return `static/js/${name}.js`;
-        },
-        chunkFileNames: `static/vendor/[hash].js`,
-        assetFileNames: (asset) => {
-          if (/\.( gif|jpeg|jpg|png|svg|webp| )$/.test(asset.name!)) {
+export default defineConfig(({mode}: any) => {
+
+  /** 開発ビルドかどうか */
+  const isDev: boolean = mode === "dev";
+
+  return {
+    root: root,
+    plugins: [
+      vue(),
+      splitVendorChunkPlugin(),
+      isDev ? checker({
+        vueTsc: true
+      }): undefined,
+    ],
+    resolve: {
+      alias: {
+        // import '/@component/sample.vue'からモジュールを使用可能にする。
+        '/@component/': path.join(__dirname, 'src/main/vue/component'),
+      }
+    },
+    server: {
+      host: true, // dockerなどの仮想から外に出すため（ポートを開けるという意味かな？）
+      port: 3000,
+      strictPort: true,
+      watch: {
+        usePolling: true
+      }
+    },
+    build: {
+      outDir: '../../src/main/resources/',
+      rollupOptions: {
+        input: entryObject,
+        output: {
+          entryFileNames: (entryFile) => {
+            /**
+             * NOTE: templatesディレクトリから一段階外に出している。
+             */
+            const name = cutFromBeginning(entryFile.name, 'templates/');
+            return `static/js/${name}.js`;
+          },
+          chunkFileNames: `static/vendor/[hash].js`,
+          assetFileNames: (asset) => {
+            if (/\.( gif|jpeg|jpg|png|svg|webp| )$/.test(asset.name!)) {
+              return 'static/assets/[name].[ext]';
+            }
+            if (/\.css$/.test(asset.name!)) {
+              return `static/css/[name].css`;
+            }
             return 'static/assets/[name].[ext]';
           }
-          if (/\.css$/.test(asset.name!)) {
-            return `static/css/[name].css`;
-          }
-          return 'static/assets/[name].[ext]';
-        }
-      },
+        },
+      }
     }
   }
 });
