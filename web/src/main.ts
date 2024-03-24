8bb8bd981830ba7ee851a6c1f0ed9e94502662bc
井上翼 <tsubasa0129ti@gmail.com>
2024-03-24 15:39:56 +0900

    feat: #13 フロントエンドのディレクトリの構成を変更した

---
+++ web/src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue';
+import './styles/style.scss';
+import App from './pages/App.vue';
+
+createApp(App).mount('#app');
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,36 +1,10 @@
 import { splitVendorChunkPlugin, defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import checker from 'vite-plugin-checker';
-
 import path from 'path';
-import fs from 'fs';
-import { glob } from 'glob';
-
-/**
- * 前処理: ビルドの出力結果を削除する。
- */
-const output = path.resolve(__dirname, '../src/main/resources');
-
-removeDir(path.resolve(output, 'templates'));
-removeDir(path.resolve(output, 'static'));
 
 /** プロジェクトルートを設定。 */
 const root = path.resolve(__dirname, 'src');
-
-/**
- * NOTE: searchPathは、src/templatesにするべき。
- * ディレクトリを含めないと、出力先をコントロールできないので、やむを得ず。。。
- */
-const searchPath = 'src';
-const entryFiles = glob
-  .sync('**/*.html', {
-    cwd: searchPath
-  })
-  .map((key) => {
-    return [removeExt(key), path.resolve(searchPath, key)];
-  });
-
-const entryObject = Object.fromEntries(entryFiles);
 
 export default defineConfig(({ mode }) => {
   /** 開発ビルドかどうか */
@@ -50,29 +24,21 @@ export default defineConfig(({ mode }) => {
     resolve: {
       alias: {
         // import '/@component/sample.vue'からモジュールを使用可能にする。
-        '/@component/': path.join(__dirname, 'src/main/vue/component')
+        '/@component/': path.join(__dirname, 'src/components')
       }
     },
     server: {
-      host: true, // dockerなどの仮想から外に出すため（ポートを開けるという意味かな？）
-      port: 3000,
       strictPort: true,
       watch: {
         usePolling: true
       }
     },
     build: {
-      outDir: '../../src/main/resources/',
+      outDir: '../dist',
+      emptyOutDir: true,
       rollupOptions: {
-        input: entryObject,
         output: {
-          entryFileNames: (entryFile) => {
-            /**
-             * NOTE: templatesディレクトリから一段階外に出している。
-             */
-            const name = cutFromBeginning(entryFile.name, 'templates/');
-            return `static/js/${name}.js`;
-          },
+          entryFileNames: `static/js/[name].js`,
           chunkFileNames: `static/vendor/[hash].js`,
           assetFileNames: (asset) => {
             if (/\.( gif|jpeg|jpg|png|svg|webp| )$/.test(asset.name!)) {
@@ -88,31 +54,3 @@ export default defineConfig(({ mode }) => {
     }
   };
 });
-
-/**
- * 指定したパスのディレクトリを再起的に削除する。
- *
- * @param path 削除する対象のディレクトリ
- */
-async function removeDir(path: string) {
-  if (fs.existsSync(path)) {
-    await fs.rm(path, { recursive: true }, () => {
-      console.log(`Removed the directory ${path} recursively.`);
-    });
-  }
-}
-
-/**
- * 対象の文字列を切り取って返却する。
- *
- * @param originalString 編集対象の文字列
- * @param targetString 切り取り対象の文字列
- * @returns 編集後の文字列
- */
-function cutFromBeginning(originalString: string, targetString: string) {
-  return originalString.substring(targetString.length);
-}
-
-function removeExt(key: string): string {
-  return key.split('.')[0];
-}
