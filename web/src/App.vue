dc584b51d5ea6ac3a6bc1ec9fc7a9166898947be
井上翼 <tsubasa0129ti@gmail.com>
2024-04-06 15:44:15 +0900

    feat: #23 不要な設定を削除した

---
+++ web/src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import axios, { AxiosResponse, AxiosError } from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:8080/api',
+  headers: {
+    'Content-Type': 'application/json',
+    'X-Requested-With': 'XMLHttpRequest'
+  },
+  responseType: 'json'
+});
+
+onMounted(() => {
+  axiosInstance
+    .get('/sample')
+    .then((response: AxiosResponse) => {
+      console.log(response);
+    })
+    .catch((err: AxiosError) => {
+      console.log(err);
+    });
+});
+</script>
+
+<template>
+  <router-link to="/">DashBoard</router-link>
+  <router-link to="/login">Login</router-link>
+  <router-view></router-view>
+</template>
+
+<style scoped></style>
--- web/src/main.ts
+++ web/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import './styles/style.scss';
-import App from './pages/App.vue';
+import App from './App.vue';
 import router from './router/index';
 
 createApp(App).use(router).mount('#app');
