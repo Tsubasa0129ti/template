2ec5b8c421a3f79d4891ef81d575a94a87f3c7e2
井上翼 <tsubasa0129ti@gmail.com>
2024-04-06 13:46:32 +0900

    feat: #23 動的ルーティングを設定した

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,10 +1,12 @@
 import { createWebHistory, createRouter } from 'vue-router';
 
 import DashBoardView from '../pages/DashBoard.vue';
+import AccountView from '../pages/Account.vue';
 import FooBar from '../pages/FooBar.vue';
 
 const routes = [
   { path: '/', component: DashBoardView },
+  { path: '/users/:id', component: AccountView },
   { path: '/foobar', component: FooBar }
 ];
 
