12105d39797ffd8315c8e5c012bd8d45b262d102
井上翼 <tsubasa0129ti@gmail.com>
2024-04-11 20:24:51 +0900

    feat: #17 不要なファイルを削除した

--- web/src/repository.d.ts
+++
@@ -1,8 +0,0 @@
-import { Repositories } from '@/apis/repositoryFactory';
-
-// TODO: 必要かどうかを確認する。
-declare module '@vue/runtime-core' {
-  export interface ComponentCustomProperties {
-    $repository: Repositories;
-  }
-}
