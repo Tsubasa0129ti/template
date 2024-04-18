c3293a4f141cc489ba3733aef5b8f1d4d0f831d4
井上翼 <tsubasa0129ti@gmail.com>
2024-04-18 21:35:49 +0900

    feat: #31 エラーの画面を作成した

--- web/src/pages/errors/error.vue
+++ web/src/pages/errors/error.vue
@@ -1,11 +1,35 @@
 <template>
-  <div>
-    <h2>ページが表示できません。</h2>
-    <p>管理者に問い合わせください。</p>
+  <header>
+    <div v-if="statusCode">{{ statusCode }} {{ responseStatus }}</div>
+    <div v-else></div>
+    <div>{{ getTime }}</div>
+  </header>
+  <div class="detail">
     <p>{{ message }}</p>
+    <p>管理者にお問合せください。</p>
   </div>
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { HttpStatusCode } from 'axios';
+
 const message = history.state.message;
+const statusCode = history.state.statusCode;
+const responseStatus = ref('');
+
+onMounted(() => {
+  if (statusCode) {
+    responseStatus.value = getResponseStatus();
+  }
+});
+
+function getTime(): string {
+  const nowTime = new Date();
+  return `${nowTime.getFullYear()}/${nowTime.getMonth() + 1}/${nowTime.getDate()} ${nowTime.getHours()}:${nowTime.getMinutes()}`;
+}
+
+function getResponseStatus(): string {
+  return HttpStatusCode[statusCode];
+}
 </script>
