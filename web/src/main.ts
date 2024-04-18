8da2e64e17671d8304528acab42d0899e929fa5f
井上翼 <tsubasa0129ti@gmail.com>
2024-04-18 21:35:14 +0900

    feat: #31 カスタムエラーを定義した

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,6 +2,11 @@ import { createApp } from 'vue';
 import './styles/style.scss';
 import App from './App.vue';
 import router from './router/index';
+import { ErrorHandlerPlugin } from './plugins/error-handler';
 
-const app = createApp(App).use(router);
+const app = createApp(App);
+
+app.use(router);
+app.use(ErrorHandlerPlugin);
+
 app.mount('#app');
---
+++ web/src/utils/custom-error.ts
@@ -0,0 +1,53 @@
+const errorMessages = {
+  INTERNAL_SERVER_ERROR: 'システム障害が発生しました。',
+  NETWORK_ERROR: 'ネットワークエラーが発生しました。しばらくしてから再度お試しください。'
+};
+type errorMessages = (typeof errorMessages)[keyof typeof errorMessages];
+
+interface CustomErrorInterface {
+  name: string;
+  message: string;
+}
+
+abstract class CustomError extends Error implements CustomErrorInterface {
+  constructor(message: string, { cause }: Error) {
+    super(message);
+    Object.defineProperty(this, 'name', {
+      configurable: true,
+      enumerable: false,
+      value: this.constructor.name,
+      writable: true
+    });
+    this.cause = cause;
+  }
+}
+
+/**
+ * Internal Server Errorを捕捉するカスタムエラーハンドラ。
+ */
+class InternalServerError extends CustomError {
+  /**
+   * カスタムエラーオブジェクトを生成する。
+   *
+   * @param error エラーオブジェクト
+   */
+  constructor(error: Error) {
+    super(errorMessages.INTERNAL_SERVER_ERROR, error);
+  }
+}
+
+/**
+ * Network Errorを捕捉するカスタムエラーハンドラ。
+ */
+class NetWorkError extends CustomError {
+  /**
+   * カスタムエラーオブジェクトを生成する。
+   *
+   * @param error エラーオブジェクト
+   */
+  constructor(error: Error) {
+    super(errorMessages.NETWORK_ERROR, error);
+  }
+}
+
+export { InternalServerError, NetWorkError };
