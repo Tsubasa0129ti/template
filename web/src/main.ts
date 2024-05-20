dca2007d1c1be1d19c4a4c35dcebc174d9d56d60
井上翼 <tsubasa0129ti@gmail.com>
2024-05-20 21:16:44 +0900

    feat: #41  FontAwesomeを追加した

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,6 +4,11 @@ import './styles/style.scss';
 import App from './App.vue';
 import router from './router/index';
 import { ErrorHandlerPlugin } from './plugins/error-handler';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(fas);
 
 const app = createApp(App);
 
@@ -11,4 +16,5 @@ app.use(store);
 app.use(router);
 app.use(ErrorHandlerPlugin);
 
+app.component('FontAwesomeIcon', FontAwesomeIcon);
 app.mount('#app');
