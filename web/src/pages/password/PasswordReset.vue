b0220b6e1738c67600eabf0b993e57a4c7b2ac89
井上翼 <tsubasa0129ti@gmail.com>
2024-05-24 22:16:46 +0900

    feat: #41 パスワードリセット画面を実装した

--- web/src/pages/password/PasswordReset.vue
+++ web/src/pages/password/PasswordReset.vue
@@ -1,3 +1,51 @@
 <template>
-  <div>Password Reset</div>
+  <CommonHeader class="header-layout" />
+  <FormLayout>
+    <template #formHeader>
+      <h1>Reset Password</h1>
+    </template>
+
+    <template #formContent>
+      <FormGroup
+        field-name="Password"
+        :required="true"
+        :error-message="passwordError"
+        class="form-group-layout"
+      />
+      <FormGroup
+        field-name="Confirm Password"
+        :required="true"
+        :error-message="confirmPasswordError"
+        class="form-group-layout"
+      />
+      <FormButton label="Reset Password" />
+    </template>
+    <template #formFooter>
+      <p>
+        <Link label="Back to DashBoard" url="/" />
+      </p>
+    </template>
+  </FormLayout>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import CommonHeader from '../components/common/CommonHeader.vue';
+import FormLayout from '../components/layout/FormLayout.vue';
+import FormGroup from '../components/form/FormGroup.vue';
+import FormButton from '../components/form/FormButton.vue';
+import Link from '../components/Link.vue';
+
+const passwordError = ref('');
+const confirmPasswordError = ref('');
+</script>
+
+<style lang="scss">
+.header-layout {
+  margin: 30px 0;
+}
+
+.form-group-layout {
+  margin-bottom: 16px;
+}
+</style>
